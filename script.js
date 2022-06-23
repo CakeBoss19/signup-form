28b5b918bb9e5361e3a83bdc7ed4e2acf8536eee
Yogii <stevenp0294@gmail.com>
2022-06-23 12:10:38 -0400

    smallscreen.css completed

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ let messageDiv = document.querySelector('.confirm');
 const message = document.createElement('p');
 
 function comparePassword(){
-    if (password2.value === password.value){
+    if (password2.value === '' && password.value === ''){
+        reset();
+    } else if (password2.value === password.value){
         borderConfirm();
     } else {
         borderDeny();
@@ -15,16 +17,17 @@ function comparePassword(){
 function borderConfirm(){
     password.style.border = '2px solid green';
     password2.style.border = '2px solid green';
-    message.classList.toggle('approved');
-    message.textContent = 'All set!';
 };
 
 function borderDeny(){
     password.style.border = '2px solid red';
     password2.style.border = '2px solid red';
-    message.classList.toggle('denied');
-    message.textContent = 'Passwords do not match';
 };
+
+function reset(){
+    password.style.border = '1px solid rgba(134, 134, 134, 0.46)';
+    password2.style.border = '1px solid rgba(134, 134, 134, 0.46)';
+}
 
 check_button.addEventListener('click', () => {
     comparePassword();
